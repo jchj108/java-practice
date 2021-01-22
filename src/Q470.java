b0262458e333555ea2cd1094a4ebc260446c56c7
jchj108 <jchj108@naver.com>
2021-01-23 01:56:33 +0900

    Q470 Math random 메소드

---
+++ src/Q470.java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+
+public class Q470 {
+
+	public static void main(String[] args) {
+
+		int value = (int) (Math.random() * 6) + 1;
+		System.out.println(value);
+	
+	}
+}				
+		
+
