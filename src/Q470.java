7c357af01596bc8dd436d16c86482d7a0c0ec7ab
jchj108 <jchj108@naver.com>
2021-01-26 02:53:10 +0900

    Q470 문자열 덧셈 출력

--- src/Q470.java
+++ src/Q470.java
@@ -8,7 +8,7 @@ public class Q470 {
 		int sum = 0;
 		
 		for(int i=0; i<str.length(); i++) {
-			
+			sum += str.charAt(i) - '0'; //문자열에서 i번째 문자 반환, 0을 빼면 유니코드에서 48이 감소되어 해당 문자 출력
 		}
 
 			System.out.println("sum="+sum);
